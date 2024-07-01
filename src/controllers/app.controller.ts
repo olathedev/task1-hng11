35a415527dd9f8952e7518f649fd061bd498c0c9
DevOlaMicheal <olapademicheal3@gmail.com>
2024-07-01 14:41:09 +0100

    weather logic

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import axios from "axios"
 import { getIp } from "../utils/getIp.utils";
 
+
+const myApiKey = 'aaac1450c370507a325bb559024a67c5'
+
 export const sayHello = async (req: Request, res: Response) => {
     // const userIp = req.ip
     const userIp = getIp(req)
@@ -9,13 +12,14 @@ export const sayHello = async (req: Request, res: Response) => {
 
     try {
         console.log(userIp)
-        const userLocation = await axios.get(`http://ip-api.com/json/102.91.49.211`)
-        // console.log(userLocation)
-
+        const {data} = await axios.get(`http://ip-api.com/json/102.91.49.211`)
+        // console.log(data)
+        const getwWeather: any = await axios.get(`http://api.weatherapi.com/v1/current.json?key=866195130645470aa6d132613240107&q=${data.city}`)
+        
         const resObj = {
             clientIp: userIp,
-            location: userLocation.data.city,
-            greeting: `hello ${userName}`
+            location: data.city,
+            greeting: `hello ${userName} the current weather is, ${getwWeather.data.current.temp_c}`,
         }
 
         res.status(200).json(resObj)
